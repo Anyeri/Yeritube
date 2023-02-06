ebac45acc7c84ad48fe8eabd629c0f678d93227b
anyeri <yerian1673@gmail.com>
2023-02-06 18:22:29 +0900

    updated

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -8,7 +8,7 @@ export default class YoutubeClient {
       baseURL: "https://www.googleapis.com/youtube/v3",
       params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
     });
-    console.log(params);
+    console.log("env::::::" + process.env.REACT_APP_YOUTUBE_API_KEY);
   }
 
   async search(params) {
